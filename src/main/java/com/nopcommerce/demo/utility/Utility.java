3580002367980ab3d5f9fc16873e76bf0308e095
primetraineefour <smith071john@gmail.com>
2020-11-21 16:56:02 +0000

    Today,s class 21/11/20

---
+++ src/main/java/com/nopcommerce/demo/utility/Utility.java
@@ -0,0 +1,42 @@
+package com.nopcommerce.demo.utility;
+
+import com.nopcommerce.demo.basepage.BasePage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+/**
+ * Created by Jay
+ */
+public class Utility extends BasePage {
+
+
+    /**
+     * This method will click on element
+     */
+    public void clickOnElement(By by){
+        driver.findElement(by).click();
+    }
+
+    public void clickOnElement(WebElement element){
+        element.click();
+    }
+
+    /**
+     * This method will send text on element
+     */
+    public void sendTextToElement(By by, String text){
+        driver.findElement(by).sendKeys(text);
+    }
+
+    public void sendTextToElement(WebElement element, String text){
+        element.sendKeys(text);
+    }
+
+    /**
+     * This method will get text on element
+     */
+    public String getTextFromElement(By by){
+        return driver.findElement(by).getText();
+    }
+
+}
---
+++ src/test/java/com/nopcommerce/demo/testsuite/LoginTest.java
@@ -0,0 +1,17 @@
+package com.nopcommerce.demo.testsuite;
+
+import com.nopcommerce.demo.testbase.TestBase;
+import org.testng.annotations.Test;
+
+/**
+ * Created by Jay
+ */
+public class LoginTest extends TestBase {
+
+
+    @Test
+    public void userShouldNavigateToLoginPageSuccessfully(){
+
+    }
+
+}
